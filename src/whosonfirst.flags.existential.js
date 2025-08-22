781422bb9bccb845dc518cb03c7b63af9712437f
thisisaaronland <thisisaaronland@localhost>
2025-08-21 18:08:36 -0700

    refactoring flags

--- src/whosonfirst.flags.existential.js
+++ src/whosonfirst.flags.existential.js
@@ -9,81 +9,163 @@
 var whosonfirst = whosonfirst || {};
 whosonfirst.flags = whosonfirst.flags || {};
 
-// This library is an adaptation of https://github.com/whosonfirst/go-whosonfirst-geojson-v2/blob/master/properties/whosonfirst/whosonfirst.go#L160-L265
-// It is designed to work with SPR results, not with full WOF feature
-// records. (20171004/dphiffer)
-
 whosonfirst.flags.existential = (function(){
+
+    /**
+     * @enum {number}
+     * @memberof whosonfirst.flags.existential
+     * @description An "existential flag" is a number between -1 (unknown), 0 (false) and 1 (true).
+     */
+    const ExistentialFlag = {
+	/** True */
+	TRUE:   1,
+	/** False */	
+	FALSE: 0,
+	/** Unknown */	
+	UNKNOWN:  -1
+    };
     
     var self = {
 	
-	'is_current': function(place){
+	/**
+	 * @function isCurrent
+	 * @memberof whosonfirst.flags.existential
+	 * @description Determine whether a Who's On First record is considered "current".
+	 * @param {Object} feature - The Who's On First record to query.
+	 * @returns {ExistentialFlag}.
+	 */	
+	isCurrent: function(feature){
+
+	    const props = feature.properties;
 	    
-	    if (typeof place['mz:is_current'] != 'undefined' &&
-		(parseInt(place['mz:is_current']) === 1 ||
-		 parseInt(place['mz:is_current']) === 0)){
-		return parseInt(place['mz:is_current']);
+	    if (typeof props['mz:is_current'] != 'undefined' &&
+		(parseInt(props['mz:is_current']) === 1 ||
+		 parseInt(props['mz:is_current']) === 0)){
+		return parseInt(props['mz:is_current']);
 	    }
-	    else if (self.is_deprecated(place) === 1 ||
-		     self.is_ceased(place) === 1 ||
-		     self.is_superseded(place) === 1){
+	    else if (self.is_deprecated(props) === 1 ||
+		     self.is_ceased(props) === 1 ||
+		     self.is_superseded(props) === 1){
 		return 0;
 	    }
 	    else {
 		return -1;
 	    }
 	},
-	
-	'is_deprecated': function(place){
+	      
+	/**
+	 * @function isDeprecated
+	 * @memberof whosonfirst.flags.existential
+	 * @description Determine whether a Who's On First record is considered "deprecated".
+	 * @param {Object} feature - The Who's On First record to query.
+	 * @returns {ExistentialFlag}.
+	 */		      
+	isDeprecated: function(feature){
+
+	    const props = feature.properties;
 	    
-	    if (! place['edtf:deprecated']){
+	    if (! props['edtf:deprecated']){
 		return 0;
 	    }
-	    else if (place['edtf:deprecated'] === 'u' ||
-		     place['edtf:deprecated'] === 'uuuu'){
+	    
+	    else if (props['edtf:deprecated'] === 'u' ||
+		     props['edtf:deprecated'] === 'uuuu'){
 		return -1;
 	    }
 	    else {
 		return 1;
 	    }
 	},
-	
-	'is_ceased': function(place){
+
+	/**
+	 * @function isCeased
+	 * @memberof whosonfirst.flags.existential
+	 * @description Determine whether a Who's On First record is considered "ceased".
+	 * @param {Object} feature - The Who's On First record to query.
+	 * @returns {ExistentialFlag}.
+	 */		      
+	isCeased: function(feature){
+
+	    const props = feature.properties;
 	    
-	    if (typeof place['edtf:cessation'] === 'undefined' ||
-		place['edtf:cessation'] === 'u' ||
-		place['edtf:cessation'] === 'uuuu'){
+	    if (typeof props['edtf:cessation'] === 'undefined' ||
+		props['edtf:cessation'] === 'u' ||
+		props['edtf:cessation'] === 'uuuu'){
 		return -1;
 	    }
-	    else if (! place['edtf:cessation']){
+	    else if (! props['edtf:cessation']){
 		return 0;
 	    }
 	    else {
 		return 1;
 	    }
 	},
-	
-	'is_superseded': function(place){
+
+	/**
+	 * @function isSuperseded
+	 * @memberof whosonfirst.flags.existential
+	 * @description Determine whether a Who's On First record has been superseding.
+	 * @param {Object} feature - The Who's On First record to query.
+	 * @returns {ExistentialFlag}.
+	 */		      	      
+	isSuperseded: function(feature){
+
+	    const props = feature.properties;
 	    
-	    if (place['wof:superseded_by'] &&
-		place['wof:superseded_by'].length > 0){
+	    if (props['wof:superseded_by'] &&
+		props['wof:superseded_by'].length > 0){
 		return 1;
 	    }
 	    else {
 		return 0;
 	    }
 	},
-	
-	'is_superseding': function(place){
+
+	/**
+	 * @function isSuperseding
+	 * @memberof whosonfirst.flags.existential
+	 * @description Determine whether a Who's On First record supersedes other records..
+	 * @param {Object} feature - The Who's On First record to query.
+	 * @returns {ExistentialFlag}.
+	 */		      	      	      
+	isSuperseding: function(feature){
+
+	    const props = feature.properties;
 	    
-	    if (place['wof:supersedes'] &&
-		place['wof:supersedes'].length > 0){
+	    if (props['wof:supersedes'] &&
+		props['wof:supersedes'].length > 0){
 		return 1;
 	    }
 	    else {
 		return 0;
 	    }
-	}
+	},
+
+        is_current: function(feature){
+	    console.warn("is_current is deprecated, use isCurrent instead.");		  
+	    return self.isCurrent(feature);
+	},
+
+        is_deprecated: function(feature){
+	    console.warn("is_deprecated is deprecated, use isDeprecated instead.");		  		  
+	    return self.isDeprecated(feature);
+	},
+
+ 	      'is_ceased': function(feature){
+	    console.warn("is_ceased is deprecated, use isCeased instead.");		  		  
+	    return self.isCeased(feature);
+	},
+	
+	      is_superseding: function(feature){
+	    console.warn("is_superseding is deprecated, use isSuperseding instead.");		  		  
+	    return self.isSuperseding(feature);
+	},
+	
+	      'is_superseded': function(feature){
+	    console.warn("is_superseded is deprecated, use isSuperseded instead.");		  		  
+	    return self.isSuperseded(feature);
+	},
+	
     };
     
     return self;
